c0c5fced9cded7017759341105986707c0c545be
Rob Heinen <bormesh@gmail.com>
2016-06-15 03:25:43 -0400

    /admin page will now work correctly -- correct validator regex module in use

--- communityprofiles/account/emailauth.py
+++ communityprofiles/account/emailauth.py
@@ -1,11 +1,11 @@
 from django.contrib.auth.backends import ModelBackend
-from django.core.validators import email_re
+from django.core.validators import EmailValidator
 from django.contrib.auth.models import User
-  
+
 class EmailBackend(ModelBackend):
     def authenticate(self, username=None, password=None):
         username = username.strip()
-        if email_re.search(username):
+        if EmailValidator.user_regex.search(username):
             try:
                 user = User.objects.get(email=username)
                 if user.check_password(password):
