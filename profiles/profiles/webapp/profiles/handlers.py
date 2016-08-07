87b4d674b24f1be8aabbea358edd28c91eeb2480
W. Matthew Wilson <matt@216software.com>
2016-08-07 14:02:55 -0400

    Removed some code from keepix

--- profiles/profiles/webapp/profiles/handlers.py
+++ profiles/profiles/webapp/profiles/handlers.py
@@ -26,8 +26,6 @@ log = logging.getLogger(__name__)
 module_template_prefix = 'profiles'
 module_template_package = 'profiles.webapp.profiles.templates'
 
-
-
 class TemplateServer(Handler):
 
     route_strings = dict({
@@ -42,102 +40,6 @@ class TemplateServer(Handler):
 
         template_name = self.route_strings[req.line_one]
         return Response.tmpl(template_name)
-
-
-class AlbumsSelect(Handler):
-
-    """
-
-    Quicker get since we don't need sorts
-    or details etc
-
-    """
-
-    route_strings = set(["GET /api/albums"])
-    route = Handler.check_route_strings
-
-    def handle(self, req):
-
-        offset = req.wz_req.args.get("offset", 0)
-        limit = req.wz_req.args.get("limit", 30)
-
-        sort_direction = req.wz_req.args.get("sort_direction", 'desc')
-
-        pgconn = self.cw.get_pgconn()
-
-        albums = [a for a in pg.albums.\
-                    Album.select_all(
-                    self.cw.get_pgconn(),
-                    offset,
-                    limit,
-                    sort_direction=sort_direction)]
-
-        total_count = pg.albums.Album.get_total_count(self.cw.get_pgconn())
-
-        return Response.json(dict(
-            message="Retrieved {0} albums".format(len(albums)),
-            reply_timestamp=datetime.datetime.now(),
-            success=True,
-            offset=offset,
-            limit=limit,
-            total_count=total_count,
-            albums=albums))
-
-
-class AlbumCategories(Handler):
-
-    route_strings = set(["GET /api/container-categories"])
-    route = Handler.check_route_strings
-
-    def handle(self, req):
-
-        return Response.json(dict(
-            message="These are all album categories",
-            reply_timestamp=datetime.datetime.now(),
-            success=True,
-            container_categories=self.get_container_category_titles()))
-
-    def get_container_category_titles(self):
-
-        qry = textwrap.dedent("""
-            select title
-            from container_categories
-            order by display_order, title
-            """)
-
-        cursor = self.cw.get_pgconn().cursor()
-
-        cursor.execute(qry)
-
-        return [row.title for row in cursor]
-
-class InsertAlbum(Handler):
-
-    route_strings = set(["POST /api/insert-new-album"])
-    route = Handler.check_route_strings
-
-    required_json_keys = [
-        "title",
-        "album_date",
-    ]
-
-    @Handler.require_json
-    @Handler.require_login
-    def handle(self, req):
-
-        new_album = pg.albums.Album.insert(
-            self.cw.get_pgconn(),
-            req.json["title"],
-            req.json["album_date"],
-            req.user.person_uuid
-        )
-
-        return Response.json(dict(
-            message="We added a new album",
-            reply_timestamp=datetime.datetime.now(),
-            success=True,
-            new_album=new_album))
-
 
 class Location(Handler):
 
@@ -157,7 +59,6 @@ class Location(Handler):
             reply_timestamp=datetime.datetime.now(),
             success=True,
             location=l))
-
 
 class AllLocations(Handler):
 
