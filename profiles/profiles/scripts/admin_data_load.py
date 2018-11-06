25a3b085ad7423a6186e3db8820703f00e9f986c
Rob Heinen <bormesh@gmail.com>
2018-11-06 15:39:41 -0500

    more work in progress data load screen now with status

--- profiles/profiles/scripts/admin_data_load.py
+++ profiles/profiles/scripts/admin_data_load.py
@@ -7,6 +7,7 @@ import logging
 import os
 import sys
 import textwrap
+import time
 
 from profiles import configwrapper
 from profiles import junkdrawer
@@ -19,6 +20,18 @@ def set_up_args():
         description="Script to watch admin data load and begin load when job is found")
 
     ap.add_argument("yaml_file_name")
+
+    ap.add_argument(
+        "--run-forever",
+        action="store_true",
+        help="Collect data, sleep, GOTO 10",
+        default=False)
+
+    ap.add_argument(
+        "--sleep-interval",
+        type=int,
+        default=1,
+        help="How many seconds to sleep for (default 1)")
 
     return ap.parse_args()
 
@@ -40,6 +53,9 @@ def lookup_job(pgconn):
 
     if cursor.rowcount:
         return cursor.fetchone()
+
+    else:
+        return None, None
 
 
 def update_job_with_total_files(pgconn, num_total_files, job_uuid):
@@ -104,19 +120,14 @@ def log_job_message(pgconn, job_uuid, message):
 
     """), dict(job_uuid=job_uuid, message=message))
 
-def load_neighborhood(csv_file_name):
+def load_neighborhood(pgconn, csv_file_name):
 
-    global pgconn
-    global cw
 
     return junkdrawer.CSVInserter.load_neighborhood(
         pgconn,
         csv_file_name)
 
-def load_cdc(csv_file_name):
-
-    global pgconn
-    global cw
+def load_cdc(pgconn, csv_file_name):
 
     return junkdrawer.CSVInserter.load_cdc(
         pgconn,
@@ -136,7 +147,6 @@ def insert_csv_files(pgconn, directory, job_uuid):
         # Skip these files
         if xls_file in set([
             "CNP Dashboard Varlist_2017.xlsx",
-
             "Indicators_Descriptions2017.xlsx",
             "CNP Dashboard Varlist_2016Apr14.xlsx",
             "CNP Dashboard Varlist_2017_w_chartlabels.xlsx",
@@ -146,42 +156,47 @@ def insert_csv_files(pgconn, directory, job_uuid):
 
             continue
 
+
         else:
 
             xls_file_path = os.path.abspath(
                 os.path.join(
                     directory,
                     xls_file))
-
-            csv_file = junkdrawer.xls2csv(xls_file_path, os.path.join(
-                "/tmp",
-                "{0}.csv".format(os.path.splitext(os.path.basename(xls_file))[0])))
-
-            csv_file_path = os.path.abspath(csv_file)
-
-
-            log_job_message(pgconn, job_uuid,
-            "<b>Trying to load: {0}</b>".\
-                format(csv_file))
-            pgconn.commit()
-
-
-            if csv_file.endswith("cdc.csv"):
-                load_cdc(csv_file_path)
-                log_job_message(pgconn, job_uuid,
-                "<span style='color:green'>Loaded cdc {0}</span>".\
-                    format(csv_file))
-
-            elif csv_file.endswith("spa.csv"):
-                load_neighborhood(csv_file_path)
-                log_job_message(pgconn, job_uuid,
-                "<span style='color:green'>Loaded neighborhood {0}</span>".\
-                    format(csv_file))
+            if 'CNP Dashboard' in xls_file_path:
+                log.info('CNP Dashboard file -- processing')
 
             else:
+
+                csv_file = junkdrawer.xls2csv(xls_file_path, os.path.join(
+                    "/tmp",
+                    "{0}.csv".format(os.path.splitext(os.path.basename(xls_file))[0])))
+
+                csv_file_path = os.path.abspath(csv_file)
+
+
                 log_job_message(pgconn, job_uuid,
-                "<span style='color:red'>Can't load {0}</span>".\
+                "<b>Trying to load: {0}</b>".\
                     format(csv_file))
+                pgconn.commit()
+
+
+                if csv_file.endswith("cdc.csv"):
+                    load_cdc(pgconn, csv_file_path)
+                    log_job_message(pgconn, job_uuid,
+                    "<span style='color:green'>Loaded cdc {0}</span>".\
+                        format(csv_file))
+
+                elif csv_file.endswith("spa.csv"):
+                    load_neighborhood(pgconn, csv_file_path)
+                    log_job_message(pgconn, job_uuid,
+                    "<span style='color:green'>Loaded neighborhood {0}</span>".\
+                        format(csv_file))
+
+                else:
+                    log_job_message(pgconn, job_uuid,
+                    "<span style='color:red'>Can't load {0}</span>".\
+                        format(csv_file))
 
             jobs_done += 1
             update_job_with_num_files_processed(pgconn, jobs_done,
@@ -189,6 +204,51 @@ def insert_csv_files(pgconn, directory, job_uuid):
 
             pgconn.commit()
 
+def do_stuff(cw):
+
+
+    job_uuid, zip_file_path = lookup_job(cw.get_pgconn())
+
+    if job_uuid is None:
+        return
+
+    log.info("Admin job found! {0}".format(job_uuid))
+
+    log_job_message(cw.get_pgconn(), job_uuid,
+        "Found open job with zip file {0}".format(zip_file_path))
+    cw.get_pgconn().commit()
+
+    import zipfile
+    zip_ref = zipfile.ZipFile(zip_file_path, 'r')
+    zip_ref.extractall('/tmp/profiles')
+
+    update_job_with_total_files(cw.get_pgconn(), len(zip_ref.infolist()),
+        job_uuid)
+
+    log_job_message(cw.get_pgconn(), job_uuid, "Extracted zip file to '/tmp' {0}".format("<br />".join([x for x in zip_ref.namelist()])))
+
+    zip_ref.close()
+
+    cw.get_pgconn().commit()
+
+    # OK, now try to do the actual data load!
+
+    # TODO -- clear our our data before we try to load!
+
+    remove_old_data(cw.get_pgconn())
+    try:
+        insert_csv_files(cw.get_pgconn(), '/tmp/profiles', job_uuid)
+
+    except Exception as e:
+        log.error("We experienced failure loading csv {0}".format(e))
+        print e
+
+    log.debug("closing admin job")
+    close_job(cw.get_pgconn(), job_uuid)
+
+    cw.get_pgconn().commit()
+
+    log.info("All done!")
 
 if __name__ == "__main__":
 
@@ -198,47 +258,23 @@ if __name__ == "__main__":
 
     cw.configure_logging("script")
 
-    pgconn = cw.get_pgconn()
+    cw.set_as_default()
 
-    job_uuid, zip_file_path = lookup_job(pgconn)
+    log.info('args {0}'.format(args))
 
-    if job_uuid is None:
-        log.info("No admin job running. Quitting")
-        sys.exit(0)
+    if args.run_forever:
+        log.info("watching and waiting for admin jobs")
 
-    log.info("Admin job found! {0}".format(job_uuid))
+        while 1:
+            do_stuff(cw)
+            time.sleep(args.sleep_interval)
 
-    log_job_message(pgconn, job_uuid,
-        "Found open job with zip file {0}".format(zip_file_path))
-    pgconn.commit()
+            cw.get_pgconn().close()
+            cw.postgresql_connection = None
 
-    import zipfile
-    zip_ref = zipfile.ZipFile(zip_file_path, 'r')
-    zip_ref.extractall('/tmp/profiles')
 
-    update_job_with_total_files(pgconn, len(zip_ref.infolist()),
-        job_uuid)
+    else:
+        do_stuff(cw)
 
-    log_job_message(pgconn, job_uuid, "Extracted zip file to '/tmp' {0}".format("<br />".join([x for x in zip_ref.namelist()])))
 
-    zip_ref.close()
 
-    pgconn.commit()
-
-    # OK, now try to do the actual data load!
-
-    # TODO -- clear our our data before we try to load!
-
-    remove_old_data(pgconn)
-    try:
-        insert_csv_files(pgconn, '/tmp/profiles', job_uuid)
-
-    except Exception as e:
-        log.error("We experienced failure loading csv {0}".format(e))
-
-    log.debug("closing admin job")
-    close_job(pgconn, job_uuid)
-
-    pgconn.commit()
-
-    log.info("All done!")
